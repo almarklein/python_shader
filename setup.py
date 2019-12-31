7f69fa64e2524ff10188781f8420ef48fdfad9f2
Almar Klein <almar.klein@gmail.com>
2019-12-31 14:40:55 +0100

    use setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import re
+
+from setuptools import find_packages, setup
+
+
+NAME = "python_shader"
+SUMMARY = "Write modern GPU shaders in Python!"
+
+with open(f"{NAME}/__init__.py") as fh:
+    VERSION = re.search(r"__version__ = \"(.*?)\"", fh.read()).group(1)
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(exclude=["tests", "tests.*", "examples", "examples.*"]),
+    python_requires=">=3.6.0",
+    license=open("LICENSE").read(),
+    description=SUMMARY,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Almar Klein",
+    author_email="almar.klein@gmail.com",
+    url="https://github.com/almarklein/python_shader",
+    zip_safe=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
